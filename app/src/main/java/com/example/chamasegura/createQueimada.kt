fa53b5fcbd6a6529ca98e84c2eaa778a7e62e383
Pedro Moura <pedromoura.projetos@gmail.com>
2024-06-03 19:04:13 +0100

    Tentativa de ligação a bd

--- app/src/main/java/com/example/chamasegura/createQueimada.kt
+++ app/src/main/java/com/example/chamasegura/createQueimada.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class createQueimada : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_criarqueimada)
+        setContentView(R.layout.activity_solicitaraprovacao)
     }
 }
---
+++ app/src/main/java/com/example/chamasegura/retrofit/tabels/Users.kt
@@ -0,0 +1,10 @@
+package com.example.chamasegura.retrofit.tabels
+
+data class Users(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val password: String,
+    val name: String,
+    val idRole: Int
+)
