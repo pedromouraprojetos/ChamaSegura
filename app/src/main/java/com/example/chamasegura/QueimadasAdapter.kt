c7d4a932b6c19cf598fde2e4f3517fbb5fc046ba
Faus11 <148981356+Faus11@users.noreply.github.com>
2024-06-09 18:15:21 +0100

    Popup do detalhe das queimadas done!

--- app/src/main/java/com/example/chamasegura/QueimadasAdapter.kt
+++ app/src/main/java/com/example/chamasegura/QueimadasAdapter.kt
@@ -1,11 +1,13 @@
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chamasegura.R
 import com.example.chamasegura.retrofit.tabels.Queimadas
-
 
 class QueimadasAdapter(private val queimadas: List<Queimadas>) : RecyclerView.Adapter<QueimadasAdapter.ViewHolder>() {
 
@@ -17,6 +19,11 @@ class QueimadasAdapter(private val queimadas: List<Queimadas>) : RecyclerView.Ad
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val queimada = queimadas[position]
         holder.bind(queimada)
+        holder.itemView.findViewById<ImageView>(R.id.arrow_icon).setOnClickListener {
+            // Capturar o clique na seta
+            // Chamar um método para exibir informações detalhadas da queimada
+            showQueimadaDetails(holder.itemView.context, queimada)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -31,5 +38,20 @@ class QueimadasAdapter(private val queimadas: List<Queimadas>) : RecyclerView.Ad
             textDate.text = queimada.date
             textStatus.text = queimada.status
         }
+    }
+
+    private fun showQueimadaDetails(context: Context, queimada: Queimadas) {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.activity_queimada_details, null)
+
+        val dialog = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .create()
+
+        dialogView.findViewById<TextView>(R.id.text_coordinates).text = "Coordenadas: (${queimada.location})"
+        dialogView.findViewById<TextView>(R.id.text_type).text = "Tipo: ${queimada.type}"
+        dialogView.findViewById<TextView>(R.id.text_date).text = "Data: ${queimada.date}"
+        dialogView.findViewById<TextView>(R.id.text_reason).text = "Motivo: ${queimada.reason}"
+
+        dialog.show()
     }
 }
